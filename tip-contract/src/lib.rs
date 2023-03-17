d4de9183e81feca698ba67ca033fdd856a51135e
mikolaj <bulmikolaj@interia.pl>
2023-03-17 10:16:38 +0100

    tip contract state tipping logic (in progress)

--- tip-contract/src/lib.rs
+++ tip-contract/src/lib.rs
@@ -1,86 +1,116 @@
 #![no_std]
 
-use soroban_sdk::{Address, Bytes, BytesN, contractimpl, contracttype, Env, log, RawVal, symbol, Symbol, vec, Vec};
+use core::iter::{Enumerate, VecIter};
+use core::ptr::null;
+use core::vec::VecIter;
+
+use soroban_sdk::{Address, Bytes, BytesN, contractimpl, contracttype, Env, log, map, Map, RawVal, symbol, Symbol, vec, Vec};
 
 const DEPLOYER_KEY: Symbol = symbol!("depl_val");
 const COUNTER: Symbol = symbol!("COUNTER");
 
 pub struct Contract;
 
+mod token {
+    soroban_sdk::contractimport!(file = "soroban_token_spec.wasm");
+}
+
 #[contracttype]
 pub enum DataKey {
-    // Address may represent a Stellar account, a contract or an 'account contract'
-    Counter(Address),
-    // Address can be used any time some network identity needs to be represented,
-    // like to distinguish between counters for different users in this example.
+    GoalDesc,
+    CurState,
+}
+
+#[contracttype]
+pub struct GoalDesc {
+    pub owner: Address,
+    pub token: BytesN<32>,
+    pub goals: Vec<i128>,
+}
+
+#[contracttype]
+pub struct CurrentState {
+    pub balance: i128,
+    pub cur_goal: u32,
+    pub transfers: Map<Address, i128>,
 }
 
 #[contractimpl]
 impl Contract {
-    pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
-        vec![&env, symbol!("Hello"), to]
+    pub fn init(env: Env, owner: Address, token: BytesN<32>, goals: Vec<i128>) {
+        verify_args(&owner, &token, &goals);
+
+        env.storage().set(&DataKey::CurState, &initial_state(&env));
+        env.storage().set(&DataKey::CurState, &GoalDesc { owner, token, goals });
     }
 
-    pub fn increment(env: Env) -> u32 {
-        env.current_contract_address();
-        let current_count: u32 = env.storage()
-            .get(&COUNTER)
-            .unwrap_or(Ok(0))
-            .unwrap();
-        log!(&env, "Current count: {}", current_count);
-        let incremented_count = current_count + 1;
-        env.storage().set(&COUNTER, &incremented_count);
-        env.events().publish((COUNTER, symbol!("increment")), incremented_count);
-        incremented_count
+    pub fn tip(env: Env, tipper: Address, amount: i128) {
+        verify_goal(&env);
+
+        let (goal_desc, cur_state) = full_state(&env);
+        let (new_state, tipped) = state_after_tip(&goal_desc, cur_state, tipper, max_transfer);
+
+        token::Client::new()
     }
 
-    pub fn auth_incr(env: Env, user: Address, value: u32) -> u32 {
-        // Requires `user` to have authorized call of the `increment` of this
-        // contract with all the arguments passed to `increment`, i.e. `user`
-        // and `value`. This will panic if auth fails for any reason.
-        // When this is called, Soroban host performs the necessary
-        // authentication, manages replay prevention and enforces the user's
-        // authorization policies.
-        // The contracts normally shouldn't worry about these details and just
-        // write code in generic fashion using `Address` and `require_auth` (or
-        // `require_auth_for_args`).
-        user.require_auth();
-        // This call is equilvalent to the above:
-        // user.require_auth_for_args((&user, value).into_val(&env));
+    pub fn verify(env: Env) {}
+}
 
-        // The following has less arguments but is equivalent in authorization
-        // scope to the above calls (the user address doesn't have to be
-        // included in args as it's guaranteed to be authenticated).
-        // user.require_auth_for_args((value,).into_val(&env));
+fn verify_args(payout_address: &Address, token: &BytesN<32>, goals: &Vec<i128>) {
+    payout_address.require_auth();
+    if goals.is_empty() {
+        panic!("Can't set up a contract with no goals!")
+    }
+}
 
-        // Construct a key for the data being stored. Use an enum to set the
-        // contract up well for adding other types of data to be stored.
-        let key = DataKey::Counter(user.clone());
+fn initial_state(env: &Env) -> CurrentState {
+    CurrentState {
+        balance: 0,
+        cur_goal: 0,
+        transfers: map![&env],
+    }
+}
 
-        // Get the current count for the invoker.
-        let mut count: u32 = env
-            .storage()
-            .get(&key)
-            .unwrap_or(Ok(0)) // If no value set, assume 0.
-            .unwrap(); // Panic if the value of COUNTER is not u32.
+fn full_state(env: &Env) -> (GoalDesc, CurrentState) {
+    let desc = env.storage().get(&DataKey::GoalDesc).unwrap().unwrap();
+    let state = env.storage().get(&DataKey::CurState).unwrap().unwrap();
+    (desc, state)
+}
 
-        // Increment the count.
-        count += value;
+fn state_after_tip(goal_desc: &GoalDesc, mut cur_state: CurrentState, tipper: Address, max_transfer: i128) -> (CurrentState, i128) {
+    let mut goals_met = 0;
+    let mut amount_left = max_transfer;
+    let mut cur_balance = cur_state.balance;
 
-        // Save the count.
-        env.storage().set(&key, &count);
-
-        // Return the count to the caller.
-        count
+    for goal in active_goals(goal_desc, &cur_state) {
+        let amount_to_meet_goal = (goal - cur_balance);
+        if amount_to_meet_goal <= amount_left {
+            amount_left -= amount_to_meet_goal;
+            goals_met += 1;
+            cur_balance = 0;
+        } else {
+            cur_balance += amount_left;
+            amount_left = 0;
+            break;
+        }
     }
 
-    pub fn init(env: Env, value: u32) {
-        env.storage().set(&DEPLOYER_KEY, &value);
-    }
+    let cur_tips = cur_state.transfers.get(tipper.clone()).unwrap_or(Ok(0)).unwrap();
+    let tipped_now = cur_tips + max_transfer - amount_left;
 
-    pub fn value(env: Env) -> u32 {
-        env.storage().get_unchecked(&DEPLOYER_KEY).unwrap()
-    }
+    cur_state.transfers.set(tipper, tipped_now);
+
+    let new_state = CurrentState {
+        balance: cur_balance,
+        cur_goal: cur_state.cur_goal + goals_met,
+        transfers: cur_state.transfers,
+    };
+
+    (new_state, tipped_now)
+}
+
+fn active_goals(goal_desc: &GoalDesc, cur_state: &CurrentState) -> VecIter<i128> {
+    goal_desc.goals.slice(cur_state.cur_goal..goal_desc.goals.len()).iter()
 }
 
 #[cfg(test)]
--- tip-contract/src/test.rs
+++ tip-contract/src/test.rs
@@ -3,83 +3,27 @@
 extern crate std;
 
 use soroban_sdk::{Address, BytesN, Env, IntoVal, symbol, vec};
-use soroban_sdk::testutils::{Address as _, Events};
+use soroban_sdk::testutils::{Address as _, Events, Logger};
 
 use super::{Contract, ContractClient};
 
 #[test]
-fn test_hello() {
-    let (env, _, client) = setup_env();
-
-    let words = client.hello(&symbol!("Dev"));
-    assert_eq!(
-        words,
-        vec![&env, symbol!("Hello"), symbol!("Dev")]
-    )
+fn test() {
+    // let (env, _, client) = setup_env();
+    //
+    // client.test();
+    // client.test2(&4i32, &5i32);
+    // let result = client.test3(&4i32);
+    //
+    // let logs = env.logger().all();
+    // std::println!("{}", logs.join("\n"));
+    // assert_eq!(
+    //     result,
+    //     5i32
+    // )
 }
 
-#[test]
-fn test_counter() {
-    let (_, _, client) = setup_env();
-
-    assert_eq!(client.increment(), 1);
-    assert_eq!(client.increment(), 2);
-    assert_eq!(client.increment(), 3);
-}
-
-#[test]
-fn test_event() {
-    let (env, contract_id, client) = setup_env();
-
-    assert_eq!(client.increment(), 1);
-    assert_eq!(client.increment(), 2);
-
-    assert_eq!(
-        env.events().all(),
-        vec![
-            &env,
-            (
-                contract_id.clone(),
-                (symbol!("COUNTER"), symbol!("increment")).into_val(&env),
-                1u32.into_val(&env)
-            ),
-            (
-                contract_id.clone(),
-                (symbol!("COUNTER"), symbol!("increment")).into_val(&env),
-                2u32.into_val(&env)
-            ),
-        ]
-    )
-}
-
-#[test]
-fn test_auth() {
-    let (env, contract_id, client) = setup_env();
-
-    let user_1 = Address::random(&env);
-    let user_2 = Address::random(&env);
-
-    assert_eq!(client.auth_incr(&user_1, &5), 5);
-
-    // Verify that the user indeed needed to authorize the call
-    assert_eq!(
-        env.recorded_top_authorizations(),
-        std::vec![(
-            // address
-            user_1.clone(),
-            contract_id.clone(),
-            // name of the function
-            symbol!("auth_incr"),
-            // arguments
-            (user_1.clone(), 5_u32).into_val(&env)
-        )]
-    );
-
-    assert_eq!(client.auth_incr(&user_2, &1), 1);
-    assert_eq!(client.auth_incr(&user_1, &1), 6);
-}
-
-pub fn setup_env() -> (Env, BytesN<32>, ContractClient) {
+fn setup_env() -> (Env, BytesN<32>, ContractClient) {
     let env = Env::default();
     let contract_id = env.register_contract(None, Contract);
     let client = ContractClient::new(&env, &contract_id);
