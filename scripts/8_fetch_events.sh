3b6765039aad42797bedfc16bd5e6082ddfcbe4e
mikolaj <bulmikolaj@interia.pl>
2023-03-31 11:29:17 +0200

    events

---
+++ scripts/8_fetch_events.sh
@@ -0,0 +1,6 @@
+# Sets up an event listener to observe events from out contract
+
+soroban events \
+    --rpc-url https://rpc-futurenet.stellar.org:443 \
+    --start-ledger <Start ledger> \
+    --id <Tip contract contract id>
